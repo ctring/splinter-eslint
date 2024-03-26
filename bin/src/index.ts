81fd9b773b957b10e8309840915abcc9b2abbbe2
Cuong Nguyen <ctring23@gmail.com>
2024-03-25 21:12:00 -0400

    Output results to file

--- bin/src/index.ts
+++ bin/src/index.ts
@@ -1,11 +1,34 @@
 #!/usr/bin/env node
 import * as fs from 'fs';
+import * as path from 'path';
 import { ESLint } from "eslint";
 import { parseArgs } from "util";
 import { glob } from "glob";
 import pluralize from "pluralize";
 
-const analyze = async (rootDir: string, include: string, exclude: string, batch: number | null) => {
+interface Result {
+  filePath: string;
+  fromLine: number;
+  toLine: number;
+  fromColumn: number;
+  toColumn: number;
+  result: any;
+}
+
+interface Output {
+  results: Result[];
+  doneFiles: string[];
+}
+
+async function analyze(
+  rootDir: string,
+  include: string,
+  exclude: string,
+  outDir: string,
+  continueFromExistingOutput: boolean,
+  batch: number | null
+) {
+  const absRootDir = path.resolve(rootDir);
   const eslint = new ESLint({
     useEslintrc: false,
     allowInlineConfig: false,
@@ -13,7 +36,7 @@ const analyze = async (rootDir: string, include: string, exclude: string, batch:
       parser: '@typescript-eslint/parser',
       parserOptions: {
         project: "**/tsconfig.json",
-        tsconfigRootDir: rootDir,
+        tsconfigRootDir: absRootDir,
       },
       plugins: ['@ctring/typeorm'],
       rules: {
@@ -23,32 +46,92 @@ const analyze = async (rootDir: string, include: string, exclude: string, batch:
     },
   });
 
-  console.log(`Finding files matching ${JSON.stringify({ rootDir, include, exclude }, null, 4)}`);
-  const files = await glob(include, { ignore: exclude, cwd: rootDir, absolute: true });
+  console.log(`Finding files matching ${JSON.stringify({ root: absRootDir, include, exclude }, null, 4)}`);
+  const files = await glob(include, { ignore: exclude, cwd: absRootDir, absolute: false });
 
   console.log(`Found ${files.length} ${pluralize("file", files.length)}`);
 
+  const results: Result[] = [];
+  const doneFiles: Set<string> = new Set();
+
+  // Load existing output if needed
+  if (continueFromExistingOutput && fs.existsSync(outDir)) {
+    const existingOutput = fs.readFileSync(outDir, 'utf8');
+    const output = JSON.parse(existingOutput) as Output;
+
+    output.results.forEach((result) => {
+      results.push(result);
+    });
+
+    output.doneFiles.forEach((file) => {
+      doneFiles.add(file);
+    });
+  }
+
   const batchSize = batch ?? files.length;
   for (let i = 0; i < files.length; i += batchSize) {
-    const fileBatch = files.slice(i, i + batchSize);
+    const unskippedFileBatch = files.slice(i, i + batchSize);
+    const fileBatch = unskippedFileBatch.filter((file) => !doneFiles.has(file));
 
+    // Compute number of skipped files
+    const skippedFiles = unskippedFileBatch.length - fileBatch.length;
+    if (skippedFiles > 0) {
+      console.info(`Skipped ${skippedFiles} ${pluralize("file", skippedFiles)}`)
+    }
+
+    // Analyze a batch of files
     const reports = await Promise.all(
       fileBatch.map(async (file) => {
-        const code = fs.readFileSync(file, 'utf8');
-        const report = await eslint.lintText(code, { filePath: file });
+        const filePath = path.join(absRootDir, file);
+        const code = await fs.promises.readFile(filePath, 'utf8');
+        const report = await eslint.lintText(code, { filePath });
         return report;
       })
     );
 
-    reports.forEach((report) => {
-      if (report[0].messages.length > 0) {
-        console.error(`Lint Report for ${report[0].filePath}:`);
-        report[0].messages.forEach((message) => {
-          console.error(`  - ${message.message} (rule: ${message.ruleId})`);
-        });
+    const tryParse = (message: string) => {
+      try {
+        return JSON.parse(message);
+      } catch (e) {
+        return null;
       }
+    }
+
+    // Process the results
+    for (const report of reports) {
+      for (const lintResult of report) {
+        const relativePath = path.relative(absRootDir, lintResult.filePath);
+        for (const message of lintResult.messages) {
+          let parsed = tryParse(message.message);
+          if (parsed) {
+            const result: Result = {
+              filePath: relativePath,
+              fromLine: message.line - 1,
+              toLine: (message.endLine || message.line) - 1,
+              fromColumn: message.column - 1,
+              toColumn: (message.endColumn || message.column) - 1,
+              result: parsed,
+            };
+            results.push(result);
+          } else {
+            console.error(message.message);
+          }
+        }
+      }
+    }
+
+    fileBatch.forEach((file) => {
+      doneFiles.add(file);
     });
 
+    // Write output
+    const output: Output = {
+      results,
+      doneFiles: Array.from(doneFiles),
+    };
+    fs.writeFileSync(outDir, JSON.stringify(output, null, 4));
+
+    // Print progress
     if (batchSize) {
       const done = Math.min(i + batchSize, files.length);
       const progressPct = Math.min((done / files.length) * 100, 100);
@@ -73,6 +156,14 @@ if (require.main === module) {
       batch: {
         type: "string",
       },
+      output: {
+        type: "string",
+        default: "messages.json"
+      },
+      continue: {
+        type: "boolean",
+        default: false,
+      }
     }
   });
 
@@ -86,8 +177,18 @@ if (require.main === module) {
   const batch = args.values.batch ? parseInt(args.values.batch) : null;
   const include = args.values.include!;
   const exclude = args.values.exclude!;
+  const output = args.values.output!;
+  const continueFromExistingOutput = args.values.continue!;
 
-  analyze(targetPath, include, exclude, batch).catch((error) => console.error(error));
+  analyze(
+    targetPath,
+    include,
+    exclude,
+    output,
+    continueFromExistingOutput,
+    batch
+  )
+    .catch((error) => console.error(error));
 }
 
 export {
