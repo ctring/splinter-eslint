a5fc2018103f3eae90e2c129be23b623f8caf7c2
Cuong Nguyen <ctring23@gmail.com>
2024-03-25 20:14:12 -0400

    Use glob to search for files

--- bin/src/index.ts
+++ bin/src/index.ts
@@ -1,84 +1,97 @@
 #!/usr/bin/env node
-import * as path from "path";
 import * as fs from 'fs';
 import { ESLint } from "eslint";
-import {
-  EntityMessage,
-  JsonMessage,
-  MethodMessage,
-} from "eslint-plugin-typeorm-analyzer/messages";
+import { parseArgs } from "util";
+import { glob } from "glob";
+import pluralize from "pluralize";
 
-const lintCodebase = async (rootDir: string) => {
+const analyze = async (rootDir: string, include: string, exclude: string, batch: number | null) => {
   const eslint = new ESLint({
     useEslintrc: false,
+    allowInlineConfig: false,
     overrideConfig: {
-        parser: '@typescript-eslint/parser',
-        parserOptions: {
-          project: true,
-        },
-        plugins: ['typeorm-analyzer'],
-        rules: {
-          "typeorm-analyzer/find-schema": "warn",
-          "typeorm-analyzer/find-api": "warn",
-        },
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: "**/tsconfig.json",
+        tsconfigRootDir: rootDir,
+      },
+      plugins: ['@ctring/typeorm'],
+      rules: {
+        "@ctring/typeorm/find-schema": "warn",
+        "@ctring/typeorm/find-api": "warn",
+      },
     },
   });
 
-  const files = await getAllFiles(rootDir);
+  console.log(`Finding files matching ${JSON.stringify({ rootDir, include, exclude }, null, 4)}`);
+  const files = await glob(include, { ignore: exclude, cwd: rootDir, absolute: true });
 
-  console.log("Linting {} files", files.length);
+  console.log(`Found ${files.length} ${pluralize("file", files.length)}`);
 
-  const reports = await Promise.all(
-    files.map(async (file) => {
-      const code = fs.readFileSync(file, 'utf8');
-      const report = await eslint.lintText(code, { filePath: file });
-      return report;
-    })
-  );
+  const batchSize = batch ?? files.length;
+  for (let i = 0; i < files.length; i += batchSize) {
+    const fileBatch = files.slice(i, i + batchSize);
 
-  reports.forEach((report) => {
-    if (report[0].messages.length > 0) {
-      console.error(`Lint Report for ${report[0].filePath}:`);
-      report[0].messages.forEach((message) => {
-        console.error(`  - ${message.message} (rule: ${message.ruleId})`);
-      });
-    }
-  });
-};
+    const reports = await Promise.all(
+      fileBatch.map(async (file) => {
+        const code = fs.readFileSync(file, 'utf8');
+        const report = await eslint.lintText(code, { filePath: file });
+        return report;
+      })
+    );
 
-async function getAllFiles(dir: string): Promise<string[]> {
-  const files = await fs.promises.readdir(dir);
-  const allFiles = await Promise.all(
-    files.map(async (file) => {
-      const filePath = path.join(dir, file);
-      console.log("Checking file:", filePath);
-      if ((await fs.promises.stat(filePath)).isDirectory()) {
-        // Skip node_modules
-        if (filePath.includes('node_modules')) {
-          return [];
-        }
-        return getAllFiles(filePath);
-      } else {
-        // Only return TypeScript files
-        if (!filePath.endsWith('.ts')) {
-          return [];
-        }
-        return filePath;
+    reports.forEach((report) => {
+      if (report[0].messages.length > 0) {
+        console.error(`Lint Report for ${report[0].filePath}:`);
+        report[0].messages.forEach((message) => {
+          console.error(`  - ${message.message} (rule: ${message.ruleId})`);
+        });
       }
-    })
-  );
-  return allFiles.flat();
-}
+    });
+
+    if (batchSize) {
+      const done = Math.min(i + batchSize, files.length);
+      const progressPct = Math.min((done / files.length) * 100, 100);
+      console.info(`Analyzing... ${progressPct.toFixed(2)}% (${done}/${files.length})`);
+    }
+  }
+};
 
 // Entry point for npx execution
 if (require.main === module) {
-  const rootDir = process.argv[2];
-  if (!rootDir) {
-    console.error('Please provide the root directory of the codebase to lint.');
+  const args = parseArgs({
+    allowPositionals: true,
+    options: {
+      include: {
+        type: "string",
+        default: "**/*.ts",
+      },
+      exclude: {
+        type: "string",
+        default: "node_modules",
+      },
+      batch: {
+        type: "string",
+      },
+    }
+  });
+
+  const targetPath = args.positionals[0];
+  if (!targetPath) {
+    console.error("Error: must provide a root dir.");
+    console.info("Usage: npx @ctring/splinter-eslint-plugin-typeorm <path>")
     process.exit(1);
   }
-  lintCodebase(rootDir)
-    .then(() => console.log('Linting complete.'))
-    .catch((error) => console.error(error));
+
+  const batch = args.values.batch ? parseInt(args.values.batch) : null;
+  const include = args.values.include!;
+  const exclude = args.values.exclude!;
+
+  analyze(targetPath, include, exclude, batch).catch((error) => console.error(error));
 }
 
+export {
+  EntityMessage,
+  JsonMessage,
+  MethodMessage,
+} from "@ctring/eslint-plugin-typeorm/messages";
